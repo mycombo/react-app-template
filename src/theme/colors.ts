f68843e8005acfa493ac7888d702afd22fe199e9
combo <fullcombo@qq.com>
2024-04-30 23:40:36 +0800

    Modify default body background color

---
+++ src/theme/colors.ts
@@ -0,0 +1,2 @@
+export const COLOR_BODY_DARK = '#121212'
+export const COLOR_BODY_LIGHT = '#fff'
--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,10 +1,20 @@
-import { extendTheme } from '@chakra-ui/react'
+import { extendTheme, StyleFunctionProps } from '@chakra-ui/react'
+import { mode } from '@chakra-ui/theme-tools'
+import { COLOR_BODY_DARK, COLOR_BODY_LIGHT } from './colors'
 
 const config = {
     initialColorMode: 'system',
     useSystemColorMode: true,
 }
 
-const theme = extendTheme({ config })
+const styles = {
+    global: (props: StyleFunctionProps) => ({
+        body: {
+            bg: mode(COLOR_BODY_LIGHT, COLOR_BODY_DARK)(props)
+        }
+    })
+}
+
+const theme = extendTheme({ config, styles })
 
 export default theme
