800437965a7da28903caeb120eee8eb3572984bd
combo <fullcombo@qq.com>
2024-05-01 10:59:30 +0800

    Modify eslint config file

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,21 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: { browser: true, es2024: true },
+  plugins: ['react-refresh'],
+  parser: '@typescript-eslint/parser',
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
-      { allowConstantExport: true },
+      {
+        allowConstantExport: true,
+        allowExportNames: ['meta', 'links', 'headers', 'loader', 'action'],
+      },
     ],
   },
 }
